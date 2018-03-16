f17156ec6acbfbe9e5e1930177efb615ed5afca5
Drew Dara-Abrams <drew@interline.io>
2018-03-16 15:27:40 -0700

    note

--- planetutils/valhalla_tilepacks.py
+++ planetutils/valhalla_tilepacks.py
@@ -3,6 +3,9 @@ from google.cloud import storage
 client = storage.Client.from_service_account_json('~/.interline-planetutils/valhalla-tilepacks-key.json') # TODO: allow override in arg or env var?
 
 bucket = client.get_bucket('valhalla-tiles') # TODO: allow override?
+
+# NOTE: https://googlecloudplatform.github.io/google-cloud-python/latest/storage/client.html
+
 # Then do other things...
 blob = bucket.get_blob('remote/path/to/file.txt')
 print(blob.download_as_string())
